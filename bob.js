d383a9ea29f72e93755eedb1fe395070a28d01c3
priyawhitehat <73846406+priyawhitehat@users.noreply.github.com>
2021-05-12 17:53:07 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,21 @@
+class bob {
+    constructor (x , y, radius){
+        var options ={
+            isStatic :false,
+            density: 0.5
+        }
+    this.body = Bodies.circle(x , y, radius , options);
+    this.radius=radius;
+    World.add(world , this.body);
+
+    }
+    display(){
+        push();
+        fill("blue");
+        circle(this.body.position.x,this.body.position.y,2*this.radius );
+        pop();
+
+    }
+    } 
+
+
